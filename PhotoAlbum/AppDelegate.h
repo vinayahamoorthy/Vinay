3b985ffdda59d7e7bf931c3366f01d29374444a0
Gavs <gavs@MacBook-Air.local>
2014-06-05 10:50:07 +0530

    Initial Commit

---
+++ PhotoAlbum/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PhotoAlbum
+//
+//  Created by Gavs on 05/06/14.
+//  Copyright (c) 2014 GAVS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhotoAlbum/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhotoAlbum
+//
+//  Created by Gavs on 05/06/14.
+//  Copyright (c) 2014 GAVS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
